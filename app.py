0c7b9b62e41e4bcc199aa206ad6acd1ebef6cb90
euguroglu <e.uguroglu@yahoo.com>
2021-01-09 17:03:23 +0300

    login and register page finished, error message added

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def index():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -34,13 +34,18 @@ def register():
 
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     return render_template('register.html', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
-
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Failed, Please check username and password', 'danger')
     return render_template('login.html', form=form)
 
 if __name__ == "__main__":
