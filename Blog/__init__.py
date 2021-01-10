9b814df9f155d462782c98530ffb214f303bc8a4
euguroglu <e.uguroglu@yahoo.com>
2021-01-10 17:42:26 +0300

    password reset and mail functionality added

--- Blog/__init__.py
+++ Blog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 #Application initiation
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'willbeaddedlater'
@@ -16,5 +17,13 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 #This is giving style to message comes from login manager
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'outgoing.itu.edu.tr'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'u@itu.edu.tr'
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_DEFAULT_SENDER'] = ''2''
+mail = Mail(app)
 
 from Blog import routes
